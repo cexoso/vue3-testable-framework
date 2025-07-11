04aad5c3c62a80fa33d2deb7a3ffbcc9d016140f
xiongjie <jiexiong@futunn.com>
2025-07-11 21:42:26 +0800

    新增删除的操作

--- src/page/todo/todo.spec.ts
+++ src/page/todo/todo.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { renderTest } from '../../render-app'
 import { mockBaseScene } from '../../test/mock/base-scene'
 import userEvent from '@testing-library/user-event'
+import { findByText } from '@testing-library/dom'
 
 describe('todo list', () => {
   it('todo list 从服务器端加载数据后能正确的展示', async () => {
@@ -18,5 +19,17 @@ describe('todo list', () => {
     await userEvent.keyboard('[enter]')
     const liList = await findAllByRole('listitem')
     expect(liList).lengthOf(3, '新增一条数据后，总 TODO 项有三条')
+  })
+  it('可以删除一条 TODO 项', async () => {
+    const { findAllByText, findAllByRole } = await renderTest({
+      initScript: mockBaseScene,
+    })
+    const list = await findAllByText('删除')
+    const first = list[0]
+    const deleteButton = await findByText(first, '删除')
+    await userEvent.click(deleteButton)
+
+    const liList = await findAllByRole('listitem')
+    expect(liList).lengthOf(1, '删除一条后只剩下一条了')
   })
 })
