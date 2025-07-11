d326ee7131dd5319b9df34eaf6b4ce316ea363ba
xiongjie <jiexiong@futunn.com>
2025-07-11 17:19:29 +0800

    首页直接用于说明

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-    {{ store.count }}
-    <button @click="index.handleClick">click</button>
-  </div>
+  <p>
+    这是一个 vue3 项目，用于展示如何构建一个可测试，开发友好的项目。
+    <br />
+    这个项目中有一个 TodoList 应用，TodoList 是用于演示如何 mock 掉请求，从行为的角度描述软件行为的.
+    <br />
+    这个项目依赖了 vue-router，采用了单页应用的方式，这为了演示在测试环境仍然可以把路由包含在内，除了
+    vue-router 外，还会依赖 vue-i18n 国标化多语言插件——用于演示 v8 引擎全局单例的害处、以及应该如何避免
+  </p>
 </template>
-<script lang="ts" setup>
-import { useIndex } from './index'
-import { storeToRefs } from 'pinia'
-const index = useIndex()
-const store = storeToRefs(index)
-</script>
